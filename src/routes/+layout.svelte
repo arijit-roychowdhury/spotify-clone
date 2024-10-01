f3f2aca970d23c86bf920f05a54ff8b6be44b27e
arijit-roychowdhury <arijit.roychowdhury.961@gmail.com>
2024-10-02 01:04:42 +0530

    Creating desktop navigation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,18 +2,19 @@
 	import 'modern-normalize/modern-normalize.css';
 	import '$styles/main.scss';
 	import type { LayoutData } from './$types';
-	import LogoutButton from '$components/LogoutButton.svelte';
+	import Navigation from '$components/Navigation.svelte';
 
 	export let data: LayoutData;
 	$: user = data.user;
 </script>
 
-{#if user}
-	<p>Hello {user.display_name}</p>
-	<LogoutButton />
-{/if}
 
 <div id="main">
+	{#if user}
+		<div id="sidebar">
+			<Navigation desktop={true} />
+		</div>		
+	{/if}
 	<div id="content">
 		<main id="main-content">
 			<slot />
@@ -23,7 +24,9 @@
 
 <style lang="scss">
 	#main {
+		display: flex;
 		#content {
+			flex: 1;
 			main#main-content {
 				padding: 30px 15px 60px;
 				@include breakpoint.up('md') {
