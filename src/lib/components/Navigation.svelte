d38a46bf27dba17e6bb25f18a0f4021bc124c63c
arijit-roychowdhury <arijit.roychowdhury.961@gmail.com>
2024-10-02 16:57:47 +0530

    Added Topbar with animation on scroll and Mobile navigation

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -3,8 +3,12 @@
 	import type { ComponentType } from "svelte";
   import logo from '$assets/Spotify_Logo_RGB_White.png';
 	import { page } from "$app/stores";
+	import { fade } from "svelte/transition";
 
-  export let desktop: Boolean;
+  export let desktop: boolean;
+
+	let isMobileMenuOpen: boolean = false;
+	$: isOpen = desktop || isMobileMenuOpen;
 
   const menuItems: {path: string, label: string, icon: ComponentType<Icon>}[] = [
     {
@@ -23,11 +27,35 @@
       icon: ListMusic,
     },
   ]; 
+
+	const openMenu = () => {
+		isMobileMenuOpen = true;
+	}
+	const closeMenu = () => {
+		isMobileMenuOpen = false;
+	}
 </script>
 
+<svelte:head>
+	{#if !desktop && isMobileMenuOpen}
+		<style>
+			body {overflow: hidden;}
+		</style>		
+	{/if}
+</svelte:head>
+
 <div class="nav-content" class:desktop class:mobile={!desktop}>
+	{#if !desktop && isMobileMenuOpen}
+		<div class="overlay" on:click={closeMenu} transition:fade={{duration: 300}} />
+	{/if}
   <nav aria-label="main">
-    <div class="nav-content-inner">
+		{#if !desktop}
+			<button on:click={openMenu}>open</button>			
+		{/if}
+    <div class="nav-content-inner" class:is-hidden={!isOpen}>
+			{#if !desktop}
+				<button on:click={closeMenu}>close</button>			
+			{/if}
       <img src={logo} class="logo" alt="Spotify" width="100px"/>
       <ul>
         {#each menuItems as item}
@@ -46,6 +74,19 @@
 
 <style lang="scss">
 	.nav-content {
+		.overlay {
+			position: fixed;
+			width: 100%;
+			height: 100%;
+			top: 0;
+			left: 0;
+			background-color: var(--sidebar-color);
+			opacity: 0.75;
+			z-index: 100;
+			@include breakpoint.up('md') {
+				display: none;
+			}
+		}
 		.logo {
 			max-width: 100%;
 			width: 130px;
@@ -94,6 +135,22 @@
 			top: 0;
 			.nav-content-inner {
 				@include breakpoint.up('md') {
+					display: block;
+				}
+			}
+		}
+		&.mobile {
+			.nav-content-inner {
+				position: fixed;
+				top: 0;
+				left: 0;
+				z-index: 100;
+				transition: transform 300ms, opacity 300ms;
+				&.is-hidden {
+					transform: translateX(-100%);
+					opacity: 0;
+				}
+				@include breakpoint.down('md') {
 					display: block;
 				}
 			}
