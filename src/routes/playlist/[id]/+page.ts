977bf79b59fc9c21f48bbc0e035d1b8837f3d5a5
arijit-roychowdhury <arijit.roychowdhury.961@gmail.com>
2024-11-05 04:33:31 +0530

    Playlist Track pagination for disable JS

--- src/routes/playlist/[id]/+page.ts
+++ src/routes/playlist/[id]/+page.ts
@@ -2,16 +2,32 @@ import fetchRefresh from '$lib/helpers/fetch-refresh';
 import { error } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
-export const load: PageLoad = async ({ fetch, params, depends, route }) => {
+export const load: PageLoad = async ({ fetch, params, depends, route, url }) => {
 	depends(`app:${route.id}`);
   const playlistRes = await fetchRefresh(fetch, `/api/spotify/playlists/${params.id}`);
   let color = null;
+	const limit = 100;
+	const page = url.searchParams.get('page');
 
 	if (!playlistRes.ok) {
 		throw error(playlistRes.status, 'Failed to load playlist!');
 	}
 
 	const playlistResJSON: SpotifyApi.SinglePlaylistResponse = await playlistRes.json();
+
+	if (page && page !== '1') {
+		const tracksRes = await fetch(
+			`/api/spotify/playlists/${params.id}/tracks?${new URLSearchParams({
+				limit: `${limit}`,
+				offset: `${limit * (Number(page) - 1)}`
+			}).toString()}`
+		);
+		if (!tracksRes.ok) {
+			throw error(tracksRes.status, 'Failed to load playlist!');
+		}
+		const tracksResJSON = await tracksRes.json();
+		playlistResJSON.tracks = tracksResJSON;
+	}
 
 	if (playlistResJSON.images.length > 0) {
 		const colorRes = await fetch(
